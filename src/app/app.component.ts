80baa5b05ed3463693276f8157755a1f5e69e60a
hmontes <hugomontes927@gmail.com>
2024-08-08 16:25:20 -0400

    29. INTERPOLACION

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LikeComponent } from './like/like.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LikeComponent],
+  imports: [LikeComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
