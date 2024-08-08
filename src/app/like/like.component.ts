99d8af747aef23e17d00a4aaaaac007096554003
hmontes <hugomontes927@gmail.com>
2024-08-08 15:47:53 -0400

    27. Archivos ASSETS

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -4,9 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-like',
   standalone: true,
   imports: [],
-  template: `<p>Like Template</p>`,
-  styles: ``
+  template: `
+    <p>Like Template</p>
+    <div class="container">
+      <img src="assets/like.png" alt="My Image" width="50" height="50">
+    </div>
+  `,
+  styles: `div {background-color: black; padding: 10px}`,
 })
-export class LikeComponent {
-
-}
+export class LikeComponent {}
