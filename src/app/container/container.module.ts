b34ce80421d7d26f6847896575eca2804e55c0f6
hmontes <hugomontes927@gmail.com>
2024-08-09 15:54:46 -0400

    37. Crear componentes para formar otro – CODIGO

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ContainerComponent } from './container.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [ContainerComponent],
+  declarations: [ContainerComponent, MenuComponent, HeaderComponent],
   exports: [ContainerComponent]
 })
 export class ContainerModule {}
