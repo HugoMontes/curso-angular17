0fe5c0cb92b8047fcca2333d98c1a04c40b02d43
hmontes <hugomontes927@gmail.com>
2024-08-12 17:52:20 -0400

    50. BEM en ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerModule } from './container/container.module';
+import { ProductModule } from './product/product.module';
+import { ProductBemModule } from './product-bem/product-bem.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContainerModule],
+  imports: [ContainerModule, ProductModule, ProductBemModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'curso-angular17';
