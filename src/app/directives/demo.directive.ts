1179fb37a58969de390a54b76477aabf98cf3bf0
hmontes <hugomontes927@gmail.com>
2024-08-15 12:44:29 -0400

    66. Un poco de Inyección de dependencias

--- src/app/directives/demo.directive.ts
+++ src/app/directives/demo.directive.ts
@@ -1,11 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDemo]',
   standalone: true,
 })
 export class DemoDirective {
-  constructor() {
+  
+  private element: ElementRef<HTMLInputElement> = inject(ElementRef);
+
+  constructor(private renderer: Renderer2) {
     console.log('--DemoDirective-->');
+    // console.log(this.element);
+    // this.element.nativeElement.style.backgroundColor = 'green';
+    renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
   }
 }
