d4de4c6dfc757f3ad5fa62c78959dfaf5d11bb3d
hmontes <hugomontes927@gmail.com>
2024-08-17 13:57:10 -0400

    78. Comunicación entre componentes - @Output

--- src/app/pokemons/pokemon/pokemon.component.ts
+++ src/app/pokemons/pokemon/pokemon.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IPokemon } from '../../models/pokemon.interface';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButton],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss',
 })
 export class PokemonComponent implements OnInit {
 
   @Input({ required: true }) pokemon?: IPokemon;
+
+  @Output() deletePokemon = new EventEmitter<IPokemon>();
 
   ngOnInit(): void {
     console.log(this.pokemon);
--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { POKEMONS } from '../models/constants';
 import { IPokemon } from '../models/pokemon.interface';
-import { PokemonComponent } from "./pokemon/pokemon.component";
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @Component({
   selector: 'app-pokemons',
@@ -17,10 +17,21 @@ export class PokemonsComponent {
   pokemons = [...POKEMONS];
   pokemonSelected?: IPokemon;
 
-  clickRestore(): void {}
+  clickRestore(): void {
+    this.pokemons = [...POKEMONS];
+  }
 
   clickItem(pokemon: IPokemon): void {
     // console.log(pokemon);
     this.pokemonSelected = pokemon;
+  }
+
+  deletePokemon(pokemon: IPokemon): void {
+    // console.log(pokemon);
+    const index = this.pokemons.indexOf(pokemon!);
+    if (index >= 0) {
+      this.pokemons.splice(index, 1);
+      this.pokemonSelected = undefined;
+    }
   }
 }
